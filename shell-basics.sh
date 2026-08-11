51147445670a4c9c86b6b2bf109139130fbddee6
Dillshad685 <dillshadb2@gmail.com>
2026-08-11 11:16:28 +0530

    firsttime

--- shell-basics.sh
+++ shell-basics.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 START_TIME=$(date +%s)
-name1=Ram
-name2=Soma
+#name1=Ram
+#name2=Soma
 
-echo "Hi $name1 I am fine what about you  $name2"
+echo "Hi $1 I am fine what about you  $2"
 echo "Please enter mobile number"
 read -s MOBILE_NUMBER
 echo "Mobile number is $MOBILE_NUMBER"
-echo "please enter pincode"
-read pin_number
-echo "enter pin code:$pin_number"
+echo "please enter pincode:$3"
+echo "enter pin code:$3"
 echo "all variables:$@"
 echo "who is running:$USER"
 echo "present working directory:$PWD"
