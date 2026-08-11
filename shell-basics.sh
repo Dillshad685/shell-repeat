11117613266a727b25bd81e1054b5121c23cd18b
Dillshad685 <dillshadb2@gmail.com>
2026-08-11 11:01:52 +0530

    firsttime

--- shell-basics.sh
+++ shell-basics.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+START_TIME=$(date +%s)
 name1=Ram
 name2=Soma
 
@@ -7,3 +8,8 @@ echo "Hi $name1 I am fine what about you  $name2"
 echo "Please enter mobile number"
 read -s MOBILE_NUMBER
 echo "Mobile number is $MOBILE_NUMBER"
+
+END_TIME=$(date +%s)
+
+echo "Script execution time: $((END_TIME - START_TIME))"
+
