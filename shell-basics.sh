3a7a246134e70e2ec08977ac302edf14eed4f3cc
Dillshad685 <dillshadb2@gmail.com>
2026-08-11 10:40:24 +0530

    firsttime

---
+++ shell-basics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name1=Ram
+name2=Soma
+
+echo "Hi $name1 I am fine what about you  $name2"
+echo "Please enter mobile number"
+read -s MOBILE_NUMBER
+echo "Mobile number is $MOBILE_NUMBER"
