545dcf2fa0afbdaf1e7d81d9d96ae73d17df5fd7
Dillshad685 <dillshadb2@gmail.com>
2026-08-11 11:08:50 +0530

    firsttime

--- shell-basics.sh
+++ shell-basics.sh
@@ -8,7 +8,15 @@ echo "Hi $name1 I am fine what about you  $name2"
 echo "Please enter mobile number"
 read -s MOBILE_NUMBER
 echo "Mobile number is $MOBILE_NUMBER"
+read pin_number
+echo "enter pin number:$pin_number"
 echo "$@"
+echo "who is running:$USER"
+echo "present working directory"$PWD"
+echo "home directory;$HOME"
+echo "PID of this script:$$"
+echo "current script name:$?"
+echo "current running script:$0"
 END_TIME=$(date +%s)
 
 echo "Script execution time: $((END_TIME - START_TIME))seconds"
