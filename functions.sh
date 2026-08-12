43c7b9705e7fc4aa13587c2d6d5bc6314aaa8090
Dillshad685 <dillshadb2@gmail.com>
2026-08-12 07:04:22 +0530

    firsttime

---
+++ functions.sh
@@ -0,0 +1,47 @@
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/shell-repeat"
+SCRIPT_PATH=$(echo $0 | cut -d "."  -f1)
+SCRIPT_NAME="$LOG_FOLDER/$SCRIPT_PATH.log"
+mkdir -p $LOG_FOLDER
+
+START_TIME=$(date +%s)
+if [ $(id -u) -eq 0 ]; then
+    echo "login with sudo users"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R error while installing $2 $N" 
+        exit 1
+    else 
+        echo -e "$G $2 installed successfully $N"
+    fi
+}
+
+dnf list installed mysql
+
+if [ $? -eq 0 ]; then
+    dnf install mysql -y &
+    VALIDATE $? "mysql"
+else
+    echo -e "$Y nginx $N installed $G successfully $N"
+fi
+
+dnf list installed nginx 
+
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo -e "$Y nginx $N installed $G successfully $N"
+fi
+
+END_TIME =$(date +%s)
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo "SCRIPT_EXECUTION time is $Y $TOTAL_TIME SEC $N"
+
